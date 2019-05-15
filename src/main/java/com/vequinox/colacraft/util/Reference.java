3fdbf17a12896d0d111052ae62ee0b140a4c2f3c
Vequinox <46204139+Vequinox@users.noreply.github.com>
2019-05-15 00:28:40 -0500

    Updated version number

--- src/main/java/com/vequinox/colacraft/util/Reference.java
+++ src/main/java/com/vequinox/colacraft/util/Reference.java
@@ -3,7 +3,7 @@ package com.vequinox.colacraft.util;
 public class Reference {
 	public static final String MOD_ID = "colacraft";
 	public static final String NAME = "Cola Craft";
-	public static final String VERSION = "0.1";
+	public static final String VERSION = "0.4.2";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.vequinox.colacraft.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.vequinox.colacraft.proxy.CommonProxy";
